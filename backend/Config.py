b2b4719c5ccd16afd7b636091a1b9f560f1262d7
Disperser <danilzaharovich@gmail.com>
2026-04-22 18:44:57 +0400

    update

--- backend/Config.py
+++ backend/Config.py
@@ -4,4 +4,3 @@
 IP = "127.0.0.1"
 PORT = 50001
 API_PORT = 8001
-AUTH_KEY = "super_secret"
--- backend/__init__.py
+++ backend/__init__.py
@@ -1,10 +1,3 @@
-from .Config import IP, PORT, AUTH_KEY, API_PORT
+from .Config import IP, PORT, API_PORT
 from .BenchUtils import add_bytes, start_benchmark
 from .Core import start_server
-
-__all__ = [
-    "IP", "PORT", "AUTH_KEY",
-    "add_bytes", "start_benchmark",
-    "start_server",
-    "API_PORT",
-]
